7ee82ffb77320c7e6d36355afbe1be95127765f3
ramiro zein <ramirozeincontrerasrangel@gmail.com>
2026-02-11 16:42:04 -0600

    Enabled Cross-Origin Resource Sharing (CORS) for `HomeController`.

--- src/main/java/computacion/marketplace/web/controller/HomeController.java
+++ src/main/java/computacion/marketplace/web/controller/HomeController.java
@@ -2,9 +2,11 @@ package computacion.marketplace.web.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
+@CrossOrigin(origins = "*")
 public class HomeController {
     @GetMapping("/")
     public String home(Model model) {
